be2ab11c26195489decefaf5b100593b2097d3e1
Sarah Mitchell <g00391704@atu.ie>
2022-10-12 12:21:11 +0100

    Added Add feature

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         multiply();
     }
-    public static void multiply()
-    {
+
+    public static void multiply() {
         System.out.println("Please enter your first number to multiply: ");
         Scanner inputs = new Scanner(System.in);
         int firstNumber = inputs.nextInt();
@@ -15,6 +14,19 @@ public class Calculator {
         int secondNumber = inputs.nextInt();
 
         int total = firstNumber * secondNumber;
-        System.out.println("The total multiplication is: "+ total);
+        System.out.println("The total multiplication is: " + total);
+    }
+
+    public static void add() {
+        System.out.println("Please enter your first number to add: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number to add: ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("The total multiplication is: " + total);
     }
 }
+
