9aaf6ec3acc729e7b6904952ab2c4821352a578d
Aleksander Kijewski <g00396018@atu.ie>
2022-10-12 12:15:02 +0100

    Added PSV to Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Calculator {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        multiply();
     }
+    public static void multiply()
+    {
+        System.out.println("Please enter your first number to multiply: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
 
+        System.out.println("Please enter your second number to multiply: ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("The total multiplication is: "+ total);
+    }
 }
